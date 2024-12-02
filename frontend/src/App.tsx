6d259442f394c5013e55280ac10f337618216f21
codypharm <williamikeji@gmail.com>
2024-12-02 12:12:26 +0100

    chore: switched to vite react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './compoments/Navbar';
-import Home from './pages/Home';
-import CoinDetailPage from './pages/Coin';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "@/components/shared/Navbar";
+import Home from "./pages/Home";
+import CoinDetailPage from "./pages/Coin";
 
 const App: React.FC = () => {
-
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-web-app.js?56";
