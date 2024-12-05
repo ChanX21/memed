2b3296aee476387cb6a35b0edb8aa2c9fa6e4959
Furkan Nabi Sumji <furkannabisumji@gmail.com>
2024-12-05 12:33:47 +0530

    bug fix

--- backend/src/middlewares/upload.ts
+++ backend/src/middlewares/upload.ts
@@ -2,14 +2,11 @@ import multer, { FileFilterCallback } from "multer";
 import { Request } from "express";
 
 const upload = multer({
-  storage: multer.memoryStorage(),
+  dest: "tmp/",
   fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only image files are allowed"));
-    }
+    file.mimetype.startsWith("image/") ? cb(null, true) : cb(new Error("Only image files are allowed"));
   },
 });
 
 export const fileMiddleware = upload.single("image");
+
