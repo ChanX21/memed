b3994b0328e184b9311568ad464b38ce4c91f0e8
codypharm <williamikeji@gmail.com>
2024-12-09 19:44:04 +0100

    market cap fix

--- frontend/src/components/home/TokenCard.tsx
+++ frontend/src/components/home/TokenCard.tsx
@@ -1,17 +1,7 @@
 import React, { useEffect, useState } from "react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { IoIosInformationCircleOutline } from "react-icons/io";
-import { Progress } from "../ui/progress";
-import { BigNumberish, formatEther, parseEther } from "ethers";
-import config from "@/config.json";
-import { useToast } from "@/hooks/use-toast";
-import { useAccount, useBalance, useReadContract } from "wagmi";
-import { Link, useParams } from "react-router-dom";
+import { BigNumberish, formatEther } from "ethers";
+import { useReadContract } from "wagmi";
+import { Link } from "react-router-dom";
 import tokenAbi from "@/abi/erc20.json";
 import {
   Card,
@@ -24,6 +14,7 @@ import {
 
 import { formatDistanceToNow } from "date-fns";
 import priceFeedAbi from "@/abi/PriceFeed.json";
+import { formatCurrency } from "@/utils";
 
 interface Props {
   coin: any;
@@ -31,7 +22,6 @@ interface Props {
 
 const TokenCard: React.FC<Props> = ({ coin }) => {
   const [marketCap, setMarketCap] = useState<number>(0);
-  const [percCompleted, setPercCompleted] = useState<number>(0);
 
   const { data: totalSupply }: { data: BigNumberish | undefined } =
     useReadContract({
@@ -46,42 +36,6 @@ const TokenCard: React.FC<Props> = ({ coin }) => {
       address: import.meta.env.VITE_BNB_FEED as `0x${string}`,
       functionName: "latestRoundData",
     });
-
-  const formatCurrency = (value: number) => {
-    const units = [
-      { threshold: 1e12, suffix: "T" }, // Trillion
-      { threshold: 1e9, suffix: "B" }, // Billion
-      { threshold: 1e6, suffix: "M" }, // Million
-      { threshold: 1e3, suffix: "K" }, // Thousand
-    ];
-
-    // Assume the value is already in the desired unit
-    const amount = value; // Directly use the number
-
-    // Find the appropriate unit
-    for (const unit of units) {
-      if (amount >= unit.threshold) {
-        const scaledValue = amount / unit.threshold;
-        return `${scaledValue.toFixed(2)} ${unit.suffix} USD`; // Adjust decimal precision as needed
-      }
-    }
-
-    // Default formatting for smaller numbers
-    return Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      maximumFractionDigits: 2,
-    }).format(amount);
-  };
-
-  useEffect(() => {
-    if (!totalSupply) return;
-    const initialSupply = 200000000;
-    const currentSupply = Number(formatEther(totalSupply));
-    const perc = ((currentSupply - initialSupply) / currentSupply) * 100;
-
-    setPercCompleted(perc);
-  }, [totalSupply]);
 
   useEffect(() => {
     const usdRate = bnbFeed && Number(bnbFeed[1]) / Math.pow(10, 8);
