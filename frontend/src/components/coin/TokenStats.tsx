f80b4326e12ddab05303a04e90b3aef27f3d29bf
codypharm <williamikeji@gmail.com>
2024-12-17 20:07:59 +0100

    stats added

--- frontend/src/components/coin/TokenStats.tsx
+++ frontend/src/components/coin/TokenStats.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useReadContract } from "wagmi";
+import React, { useEffect, useState } from "react";
+import { useReadContract, usePublicClient, useWatchContractEvent } from "wagmi";
 import { useParams } from "react-router-dom";
 import { BattleChart } from "./BattleChart";
 import { GrTransaction } from "react-icons/gr";
@@ -14,6 +14,11 @@ import memedBattle from "@/abi/memedBattle.json";
 import config from "@/config.json";
 import { BigNumberish, formatEther } from "ethers";
 import tokenAbi from "@/abi/erc20.json";
+import eventsAbi from "@/abi/events.json";
+import { AbiEvent } from "viem";
+import { decodeEventLog } from "viem";
+import { watchContractEvent } from "viem/actions";
+import { config as wagmiConfig } from "@/wagmi";
 
 interface Props {
   supply: bigint;
@@ -53,9 +58,21 @@ type TokenData = [
   bigint, // createdAt (uint256)
 ];
 
+interface DecodedLog {
+  args: {
+    amount: BigNumberish;
+    buyer: string;
+    token: string;
+    totalPrice: BigNumberish;
+  };
+}
+
 const TokenStats: React.FC = () => {
   const { tokenAddress } = useParams<{ tokenAddress: string }>();
   const [percCompleted, setPercCompleted] = useState<number>(0);
+  const [sells, setSells] = useState<number>(0);
+  const [buys, setBuys] = useState<number>(0);
+  const publicClient = usePublicClient();
 
   const { data }: { data: BattleStatsArray | undefined } = useReadContract({
     abi: memedBattle.abi,
@@ -87,6 +104,135 @@ const TokenStats: React.FC = () => {
       args: [tokenAddress, totalSupply], // Arguments: token address and its total supply
     });
 
+  //watch buy event
+  useWatchContractEvent({
+    address: config.address as `0x${string}`,
+    abi: config.abi,
+    eventName: "TokensBought",
+    onLogs(logs) {
+      console.log("New logs!", logs);
+      const totalPrice =
+        logs?.reduce(
+          //@ts-ignore
+          (accumulator: number, currentItem: DecodedLog) => {
+            const priceInEth = parseFloat(
+              formatEther(currentItem.args.totalPrice),
+            );
+            return accumulator + priceInEth;
+          },
+          0,
+        ) || 0;
+
+      //@ts-ignore
+      setBuys(totalPrice + buys);
+    },
+  });
+
+  //watch sell event
+  useWatchContractEvent({
+    address: config.address as `0x${string}`,
+    abi: config.abi,
+    eventName: "TokensSold",
+    onLogs(logs) {
+      console.log("New logs!", logs);
+      const totalPrice =
+        logs?.reduce(
+          //@ts-ignore
+          (accumulator: number, currentItem: DecodedLog) => {
+            const priceInEth = parseFloat(
+              formatEther(currentItem.args.totalPrice),
+            );
+            return accumulator + priceInEth;
+          },
+          0,
+        ) || 0;
+
+      //@ts-ignore
+      setSells(totalPrice + sells);
+    },
+  });
+
+  //fetch buy logs
+  const fetchBuyLogs = async () => {
+    try {
+      const logs = await publicClient?.getLogs({
+        address: config.address as `0x${string}`,
+        event: eventsAbi.tokensBought as AbiEvent,
+        fromBlock: 0n, // Start block
+        toBlock: "latest", // End block
+        args: {
+          token: tokenAddress, // Filter by indexed argument
+        },
+      });
+
+      const decodedLogs = logs?.map((log) =>
+        decodeEventLog({
+          abi: [eventsAbi.tokensBought], // Pass the ABI array
+          data: log.data,
+          topics: log.topics,
+        }),
+      );
+
+      const totalPrice =
+        decodedLogs?.reduce(
+          //@ts-ignore
+          (accumulator: number, currentItem: DecodedLog) => {
+            const priceInEth = parseFloat(
+              formatEther(currentItem.args.totalPrice),
+            );
+            return accumulator + priceInEth;
+          },
+          0,
+        ) || 0;
+      setBuys(totalPrice as number);
+    } catch (error) {
+      console.error("Error fetching logs:", error);
+    }
+  };
+
+  //fetch buy logs
+  const fetchSellLogs = async () => {
+    try {
+      const logs = await publicClient?.getLogs({
+        address: config.address as `0x${string}`,
+        event: eventsAbi.tokensSold as AbiEvent,
+        fromBlock: 0n, // Start block
+        toBlock: "latest", // End block
+        args: {
+          token: tokenAddress, // Filter by indexed argument
+        },
+      });
+
+      const decodedLogs = logs?.map((log) =>
+        decodeEventLog({
+          abi: [eventsAbi.tokensSold], // Pass the ABI array
+          data: log.data,
+          topics: log.topics,
+        }),
+      );
+
+      const totalPrice =
+        decodedLogs?.reduce(
+          //@ts-ignore
+          (accumulator: number, currentItem: DecodedLog) => {
+            const priceInEth = parseFloat(
+              formatEther(currentItem.args.totalPrice),
+            );
+            return accumulator + priceInEth;
+          },
+          0,
+        ) || 0;
+      setSells(totalPrice as number);
+    } catch (error) {
+      console.error("Error fetching logs:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBuyLogs();
+    fetchSellLogs();
+  }, [publicClient]);
+
   return (
     <div className="grid h-full grid-cols-1 lg:grid-cols-2 px-3 gap-4">
       <div className="h-full flex w-full flex-col justify-center ">
@@ -95,21 +241,21 @@ const TokenStats: React.FC = () => {
             <GrTransaction size={25} className="text-gray-500" />
             <p>Total transactions</p>
           </div>
-          <div className="font-semibold">1000</div>
+          <div className="font-semibold">{sells + buys}</div>
         </div>
         <div className="flex justify-between py-3 border-b border-gray-500">
           <div className="flex items-center gap-3">
             <GiReceiveMoney size={25} className="text-gray-500" />
             <p> Sells</p>
           </div>
-          <div className="font-semibold">1000</div>
+          <div className="font-semibold">{sells}</div>
         </div>
         <div className="flex justify-between py-3 border-b border-gray-500">
           <div className="flex items-center gap-3">
             <GiPayMoney size={25} className="text-gray-500" />
             <p> Buys</p>
           </div>
-          <div className="font-semibold">1000</div>
+          <div className="font-semibold">{buys}</div>
         </div>
         <div className="flex justify-between py-3 border-b border-gray-500">
           <div className="flex items-center gap-3">
@@ -135,18 +281,14 @@ const TokenStats: React.FC = () => {
             <AiOutlineLineChart size={25} className="text-gray-500" />
             <p> Battles</p>
           </div>
-          <div className="font-semibold">
-            {data ? Number(data[1]) : "Loading..."}
-          </div>
+          <div className="font-semibold">{data ? Number(data[1]) : 0}</div>
         </div>
         <div className="flex justify-between py-3 border-b border-gray-500">
           <div className="flex items-center gap-3">
             <MdFingerprint size={25} className="text-gray-500" />
             <p> Votes</p>
           </div>
-          <div className="font-semibold">
-            {data ? Number(data[3]) : "Loading..."}
-          </div>
+          <div className="font-semibold">{data ? Number(data[3]) : 0}</div>
         </div>
       </div>
 
